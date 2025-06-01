5d8a4db75ea9a6ed830b5eccfeae1f6381296d91
J Cestariolli <jessica.cestariolli@gmail.com>
2025-06-01 12:55:44 -0300

    refactor: organizing pkgs, style and architecture

--- cmd/spotify-integration/main.go
+++ cmd/spotify-integration/main.go
@@ -11,13 +11,11 @@ import (
 func main() {
 	var config app.Config
 	if loadConfig(&config) != true {
-		fmt.Println(" ----> Ending program.")
 		return
 	}
 	spotifyClient := loadSpotifyClient(config)
 
 	if runApp(spotifyClient) != true {
-		fmt.Println(" ----> Ending program.")
 		return
 	}
 }
@@ -26,35 +24,38 @@ func loadConfig(configPtr *app.Config) bool {
 	fmt.Println("Loading app configs...")
 	config, err := app.Load()
 	if err != nil {
-		fmt.Printf("Error loading app configs: %s\n\n", err.Error())
-		fmt.Println(" ----> Ending program.")
+		fmt.Println("Error loading app configs :(")
+		fmt.Printf("----> %s\n\n", err.Error())
 		return false
 	}
 	*configPtr = config
-	fmt.Print("App config loaded with success!\n\n\n")
+	fmt.Println("Configs loaded with success! :)")
 	return true
 }
 
 func loadSpotifyClient(config app.Config) client.SpotifyClient {
+	fmt.Println("Loading spotify client...")
 	spotifyConfig := config.Clients.Spotify
 	spotifyClient := client.NewSpotifyClient(
 		spotifyConfig.BaseUrl,
 		spotifyConfig.AccountsUrl,
 		spotifyConfig.ClientCredentials,
 	)
+	fmt.Println("Client loaded! :)")
 	return spotifyClient
 }
 
 func runApp(spotifyClient client.SpotifyClient) bool {
-	fmt.Println("Trying authenticate with client credentials...")
+	fmt.Println("Trying to authenticate with client credentials...")
 	oAuthResponse, err := spotifyClient.AuthenticateWithClientCredentials()
 	if err != nil {
-		fmt.Printf("Error authenticating: %s\n\n", err.Error())
+		fmt.Println("Authentication failed :(")
+		fmt.Printf("----> %s\n\n", err.Error())
 		return false
 	}
-	fmt.Println("Authentication succeeded!")
+	fmt.Println("Authentication succeeded! :)")
 	if body, err3 := json.Marshal(oAuthResponse); err3 == nil && body != nil {
-		fmt.Println(string(body))
+		fmt.Printf("----> %s\n\n", string(body))
 	}
 	return true
 }
