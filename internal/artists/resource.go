cdd455d2829d0a7b8b0206624db63da23c60dcc0
J Cestariolli <jessica.cestariolli@gmail.com>
2025-06-07 00:24:42 -0300

    chore: make artists resource use http utils

--- internal/artists/resource.go
+++ internal/artists/resource.go
@@ -1,12 +1,9 @@
 package artists
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/samber/lo"
-	"io"
-	"jezz-go-spotify-integration/internal/commons"
 	"jezz-go-spotify-integration/internal/model"
+	"jezz-go-spotify-integration/internal/utils"
 	"net/http"
 	"strings"
 )
@@ -29,7 +26,7 @@ func NewResource(
 }
 
 func (a Resource) Get(accessToken model.AccessToken, artistId string) (model.Artist, error) {
-	req, cErr := a.createRequest("GET", "/"+artistId, map[string]string{}, accessToken)
+	req, cErr := utils.CreateHttpRequest(a.baseUrl+apiVersion+artistsResource, "GET", "/"+artistId, map[string]string{}, accessToken)
 	if cErr != nil {
 		return model.Artist{}, fmt.Errorf("error creating artist request for astist ID - %s - %w", artistId, cErr)
 	}
@@ -39,14 +36,14 @@ func (a Resource) Get(accessToken model.AccessToken, artistId string) (model.Art
 		return model.Artist{}, fmt.Errorf("error connecting to artist client for astist ID - %s - %w", artistId, reqErr)
 	}
 
-	if vErr := a.validateReqSuccess(resp); vErr != nil {
+	if vErr := utils.ValidateHttpResponseStatus(resp); vErr != nil {
 		return model.Artist{}, vErr
 	}
-	artist, pErr := a.parseSingleArtistResponse(resp)
-	if pErr != nil {
+	output := &model.Artist{}
+	if pErr := utils.ParseHttpResponse(resp, output); pErr != nil {
 		return model.Artist{}, fmt.Errorf("error parsing response from resource for astist ID - %s - %w", artistId, pErr)
 	}
-	return artist, nil
+	return *output, nil
 }
 
 func (a Resource) GetBatch(accessToken model.AccessToken, artistsIds ...string) (model.Artists, error) {
@@ -58,7 +55,7 @@ func (a Resource) GetBatch(accessToken model.AccessToken, artistsIds ...string) 
 		"ids": artistsIdsStr,
 	}
 
-	req, cErr := a.createRequest("GET", "", queryParameters, accessToken)
+	req, cErr := utils.CreateHttpRequest(a.baseUrl+apiVersion+artistsResource, "GET", "", queryParameters, accessToken)
 	if cErr != nil {
 		return model.Artists{}, fmt.Errorf("error creating artist request for astists IDs - %s - %w", artistsIdsStr, cErr)
 	}
@@ -68,15 +65,15 @@ func (a Resource) GetBatch(accessToken model.AccessToken, artistsIds ...string) 
 		return model.Artists{}, fmt.Errorf("error connecting to artist client for astists IDs - %s - %w", artistsIdsStr, reqErr)
 	}
 
-	if vErr := a.validateReqSuccess(resp); vErr != nil {
+	if vErr := utils.ValidateHttpResponseStatus(resp); vErr != nil {
 		return model.Artists{}, vErr
 	}
 
-	artists, pErr := a.parseMultipleArtistsResponse(resp)
-	if pErr != nil {
-		return model.Artists{}, fmt.Errorf("error parsing response from resource  for astists ID - %s - %w", artistsIdsStr, pErr)
+	output := &model.MultipleArtists{}
+	if pErr := utils.ParseHttpResponse(resp, output); pErr != nil {
+		return model.Artists{}, fmt.Errorf("error parsing response from resource for astists ID - %s - %w", artistsIdsStr, pErr)
 	}
-	return artists, nil
+	return output.Artists, nil
 
 }
 
@@ -85,97 +82,4 @@ func (a Resource) validateArtistsIdSize(artistIds []string) error {
 		return fmt.Errorf("error getting artist - artist id must not be null")
 	}
 	return nil
-}
-
-func (a Resource) createRequest(method string, path string, queryParams map[string]string, accessToken model.AccessToken) (*http.Request, error) {
-	url := a.baseUrl + apiVersion + artistsResource
-	if path != "" {
-		url += path
-	}
-	if len(queryParams) > 0 {
-		url += "?" + strings.Join(
-			lo.MapToSlice(queryParams, func(key string, value string) string {
-				return key + "=" + value
-			}),
-			"&",
-		)
-	}
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+accessToken.String())
-	return req, err
-}
-
-func (a Resource) validateReqSuccess(resp *http.Response) *commons.ResourceError {
-	if resp.StatusCode != 200 {
-		apiErr := commons.ResourceError{
-			Status:  resp.StatusCode,
-			Message: "error in artists API",
-		}
-		respBody, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return &apiErr
-		}
-
-		_ = json.Unmarshal(respBody, &apiErr)
-		return &apiErr
-	}
-	return nil
-}
-
-func (a Resource) parseMultipleArtistsResponse(resp *http.Response) (model.Artists, error) {
-	defer func(body io.ReadCloser) {
-		_ = body.Close()
-	}(resp.Body)
-
-	multipleArtists := model.MultipleArtists{}
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return multipleArtists.Artists, err
-	}
-
-	if err = json.Unmarshal(respBody, &multipleArtists); err != nil {
-		var apiErr commons.ResourceError
-		if err2 := json.Unmarshal(respBody, &apiErr); err2 == nil && apiErr.Message != "" {
-			return multipleArtists.Artists, apiErr
-		}
-		var errMessage string
-		if len(multipleArtists.Artists) < 1 {
-			errMessage = ": empty artists list (response body: " + string(respBody) + " )"
-		} else {
-			errMessage = ", no details were provided"
-		}
-		return multipleArtists.Artists, commons.AppError{
-			Code:    resp.Status,
-			Message: "error obtaining artists" + errMessage,
-		}
-	}
-	return multipleArtists.Artists, nil
-}
-
-func (a Resource) parseSingleArtistResponse(resp *http.Response) (model.Artist, error) {
-	defer func(body io.ReadCloser) {
-		_ = body.Close()
-	}(resp.Body)
-
-	artist := model.Artist{}
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return artist, err
-	}
-
-	if err = json.Unmarshal(respBody, &artist); err != nil {
-		var apiErr commons.ResourceError
-		if err2 := json.Unmarshal(respBody, &apiErr); err2 == nil && apiErr.Message != "" {
-			return artist, apiErr
-		}
-		return artist, commons.AppError{
-			Code:    resp.Status,
-			Message: "error obtaining artists, no details were provided",
-		}
-	}
-	return artist, nil
 }
