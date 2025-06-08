cc1a45b4eba22c44a0621e2d85c3b13a372671a2
J Cestariolli <jessica.cestariolli@gmail.com>
2025-06-08 14:19:43 -0300

    feat: add pagination model types

---
+++ internal/model/pagination.go
@@ -0,0 +1,11 @@
+package model
+
+type Limit int
+
+type Offset int
+
+type Total int
+
+type Next string
+
+type Previous string
--- internal/model/track.go
+++ internal/model/track.go
@@ -21,10 +21,10 @@ type SimplifiedTrack struct {
 
 type Tracks struct {
 	Href     Href              `json:"href"`
-	Limit    int               `json:"limit"`
-	Next     string            `json:"next"`
-	Offset   int               `json:"offset"`
-	Previous string            `json:"previous"`
-	Total    int               `json:"total"`
+	Limit    Limit             `json:"limit"`
+	Next     Next              `json:"next"`
+	Offset   Offset            `json:"offset"`
+	Previous Previous          `json:"previous"`
+	Total    Total             `json:"total"`
 	Items    []SimplifiedTrack `json:"items"`
 }
